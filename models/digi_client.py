4f63f09753db476384a6bf10adae4460ad610fd4
Jur de Vries <jur.devries@simplicate.nl>
2024-04-12 13:59:50 +0200

    Add test for sending correct product payload to Digi
    A new test function has been created to ensure the correct payload is sent when transferring product data to Digi. Additionally, the sending product function now prepares the data according to Digi's specific requirements. These changes provide a more detailed and accurate verification of the product transfer process.

--- models/digi_client.py
+++ models/digi_client.py
@@ -20,5 +20,23 @@ class DigiClient(models.Model):
             "Content-Type": "application/json",
         }
         url = "https://fresh.digi.eu:8010/API/V1/ARTICLE.SVC/POST"
-        body = json.dumps([])
+
+        data = {}
+        data["DataId"] = product.plu_code
+        data["Names"] = [
+            {
+                "Reference": "Nederlands",
+                "DdFormatCommodity": f"01000000{product.name}",
+                "DdFormatIngredient": f"01000000{product.ingredients}",
+            }
+        ]
+        if product.list_price:
+            data["UnitPrice"] = int(product.list_price * 100)
+        if product.standard_price:
+            data["CostPrice"] = int(product.standard_price * 100)
+        if product.categ_id.id:
+            data["MainGroupDataId"] = product.categ_id.id
+        data["StatusFields"] = {"PiecesArticle": False}
+
+        body = json.dumps(data)
         requests.post(url=url, headers=headers, data=body, timeout=30)
--- tests/test_digi_client.py
+++ tests/test_digi_client.py
@@ -5,6 +5,10 @@ from odoo.tests import TransactionCase, tagged
 
 
 class DigiClientTestCase(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.maxDiff = None
+
     @tagged("post_install", "-at_install")
     def test_it_can_be_instantiated_with_username_and_password(self):
         digi_client = self.env["digi_sync.digi_client"].create(
@@ -36,3 +40,68 @@ class DigiClientTestCase(TransactionCase):
 
             self.assertEqual(post_spy.call_args.kwargs["url"], expected_url)
             self.assertEqual(post_spy.call_args.kwargs["headers"], expected_headers)
+
+    @tagged("post_install", "-at_install")
+    def test_it_sends_a_product_to_digi_with_the_right_payload(self):
+        digi_client = self.env["digi_sync.digi_client"].create(
+            {"username": "test_username", "password": "123"}
+        )
+
+        name = "Test product"
+        ingredients = "Noten en zo"
+        plu_code = 200
+        expected_unit_price = 250
+        expected_cost_price = 150
+
+        test_category = self.env["product.category"].create({"name": "Test category"})
+
+        expected_payload = self._create_expected_payload(
+            expected_cost_price,
+            expected_unit_price,
+            ingredients,
+            name,
+            plu_code,
+            test_category.id,
+        )
+
+        product = self.env["product.product"].create(
+            {
+                "name": "Test product",
+                "ingredients": ingredients,
+                "plu_code": plu_code,
+                "categ_id": test_category.id,
+                "list_price": 2.5,
+                "standard_price": 1.5,
+            }
+        )
+
+        with patch("requests.post") as post_spy:
+            post_spy.return_value.status_code = 200
+
+            digi_client.send_product_to_digi(product)
+
+            self.assertEqual(post_spy.call_args.kwargs["data"], expected_payload)
+
+    def _create_expected_payload(
+        self,
+        expected_cost_price,
+        expected_unit_price,
+        ingredients,
+        name,
+        plu_code,
+        category_id,
+    ):
+        data = {}
+        data["DataId"] = plu_code
+        data["Names"] = [
+            {
+                "Reference": "Nederlands",
+                "DdFormatCommodity": f"01000000{name}",
+                "DdFormatIngredient": f"01000000{ingredients}",
+            }
+        ]
+        data["UnitPrice"] = expected_unit_price
+        data["CostPrice"] = expected_cost_price
+        data["MainGroupDataId"] = category_id
+        data["StatusFields"] = {"PiecesArticle": False}
+        return json.dumps(data)
